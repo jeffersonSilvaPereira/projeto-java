a3e4cf2204d37170e0f630936cb125318f0f2121
jeffersonSilvaPereira <jeffplay454@gmail.com>
2024-09-26 21:05:47 -0300

    Entra do nome do Usuario

--- src/App.java
+++ src/App.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("olá,mundo!");
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Digite seu nome:");
+        String nome = sc.nextLine();
+
+        sc.close();
+
+        System.out.printf("Olá, %s!", nome);
     }
 }
